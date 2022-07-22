b354d5efe45b1f294b06d42624b186d10660b97c
Herman Franco <95980004+HermFranco@users.noreply.github.com>
2022-07-21 19:28:48 -0500

    Update index.js

--- Javascript/02/index.js
+++ Javascript/02/index.js
@@ -1,13 +1,13 @@
 const persona = {
-    nombre: "rama",
-    edad: 40,
+    nombre: "Herman",
+    edad: 36,
     isDeveloper: true,
-    "fecha-nacimiento": new Date("02, 02, 1981"),
+    "fecha-nacimiento": new Date("06, 05, 1986"),
     "libro-favorito": {
-      titulo: "El mundo de sofia",
-      autor: "Jostein Gaarder",
-      fecha: 1991,
-      url: "https://www.amazon.es/mundo-Sof%C3%ADa-Edades-Biblioteca-Gaarder/dp/8498414512"
+      titulo: "Digital fortress",
+      autor: "Dann Brown",
+      fecha: 1998,
+      url: "https://www.amazon.com.mx/Digital-Fortress-Dan-Brown/dp/0312944926"
     }
   }
   console.log(persona["fecha-nacimiento"]);
